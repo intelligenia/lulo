1514eb86d3932df0a9640daa6c7382f1c9abec4d
Francisco Morales <franmorales@gmail.com>
2016-05-26 14:28:51 +0200

    creando un init.php que inicialice las clases de luloquery que es llamado desde el init de lulomodel

--- init.php
+++ init.php
@@ -1,7 +1,7 @@
 <?php
 
 define("LULO_DIR", __DIR__);
-define("PARENT_LULO_DIR", __DIR__."/..");
+define("PARENT_LULO_DIR", __DIR__ . "/lib");
 
 define("LULO_DIR__CONTAINERS", LULO_DIR."/containers");
 define("LULO_DIR__DB", LULO_DIR."/db");
@@ -11,15 +11,15 @@ define("LULO_DIR__MODELS", LULO_DIR."/models");
 define("LULO_DIR__QUERY", LULO_DIR."/query");
 define("LULO_DIR__TESTS", LULO_DIR."/tests");
 
-require_once LULO_DIR."/configuration.php";
+require_once LULO_DIR . "/configuration.php";
 
-require_once LULO_DIR__DB."/db.class.php";
+require_once LULO_DIR__DB . "/db.class.php";
 
-require_once LULO_DIR__CONTAINERS."/collection.class.php";
-require_once LULO_DIR__QUERY."/twigtemplate.class.php";
+require_once LULO_DIR__CONTAINERS . "/collection.class.php";
+require_once LULO_DIR__QUERY . "/twigtemplate.class.php";
 
-require_once LULO_DIR__MODELS."/init.php";
-
+require_once LULO_DIR__MODELS . "/init.php";
+require_once LULO_DIR__QUERY . "/init.php";
 
 
 
