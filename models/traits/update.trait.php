cbca63be55d44e53d453f6e2c7a44215fbccc1ce
Diego J <diego@intelligenia.com>
2016-05-26 20:13:38 +0200

    Update trait reviewed

--- models/traits/update.trait.php
+++ models/traits/update.trait.php
@@ -12,85 +12,82 @@ namespace lulo\models\traits;
 trait Update {
 	
 	/**
-	 * Actualiza todos los objetos de la base de datos.
-	 * @param $condition Condición de actualización.
-	 * @param $values Valores a modificar. En formato campo=>valor.
-	 * @return true si la actualización ha sido un éxito, false en otro caso.
+	 * Update all objects that match with the condition.
+	 * @param $condition Update condition.
+	 * @param $values Update values. Array of pairs field => new_value.
+	 * @return true if it the update was successful, false otherwise.
 	 * */
 	public static function dbUpdateAll($condition, $values){
 		$db = static::DB;
-		// Actualización de los campos del objeto
 		$ok = $db::updateFields(static::TABLE_NAME, $values, $condition);
 		return $ok;
 	}
 	
 	
 	/**
-	 * Actualiza el objeto actual en la base de datos.
-	 * @param $values Valores a modificar. En formato campo=>valor.
-	 * @return true si la actualización ha sido un éxito, false en otro caso.
+	 * Update current object.
+	 * @param $values Update values. Array of pairs field => new_value.
+	 * @return true if it the update was successfull, false otherwise.
 	 * */
 	public function dbUpdate($values){
 		$db = static::DB;
-		// Clave primaria del objeto
+		// Primary key of this object
 		$pkValue = $this->getPk();
-		// Actualización del objeto
+		// Updating the object
 		$this->setAttributes($values);
-		// Actualización de los campos del objeto
+		// Update object in table
 		$ok = $db::updateFields(static::TABLE_NAME, $values, $pkValue);
 		return $ok;
 	}
 	
 	
 	/**
-	 * Escribe un blob cuyo nombre de atributo es $blobName.
-	 * @param $blobName Nombre del blob.
-	 * @param $mixed Objeto blob en varios formatos.
+	 * Stores the blob $blobName.
+	 * @param $blobName Name of blob attribute.
+	 * @param $mixed Blob object, several formats allowed.
+	 * @return true if it the update was successfull, false otherwise.
 	 * */
 	public function dbBlobUpdate($blobName, $blob){
 		$db = static::DB;
 		
-		// Condición de este objeto
+		// Primary key of this object
 		$condition = $this->getPk();
 		
-		// 1. Si es una cadena, es el binario del fichero
+		// 1. If $blob is a string, take it as a string
 		if(is_string($blob)){
 			$blobResource = $blob;
 		}
-		// 2. Si es una array, y tiene la clave "path",
-		// es la ruta de un fichero 
+		// 2. If $blob is an array and has the key "path",
+		// $blob["path"] is the path of the file whose contents
+		// we must store for $blobName
 		elseif(is_array($blob)){
-			// Si tiene el atributo path es la ruta del binario subido,
-			// seguramente en /tmp
 			if(isset($blob["path"])){
 				$blobResource = file_get_contents($blob["path"]);
 			}else{
-				throw new \UnexpectedValueException("El blob {$blobName} es un array pero no tiene 'path' como clave.");	
+				throw new \UnexpectedValueException("Blob {$blobName} needs key 'path'.");	
 			}
 		}
-		// 3. Es NULL
+		// 3. Is NULL
 		elseif(is_null($blob)){
-			// Lo primero es comprobar si el blob es nullable, si no es nullable
-			// lanzamos una excepción
+			// If attribute is not nullable, thrown an exception
 			if(!isset(static::$ATTRIBUTES[$blobName]["null"]) or !static::$ATTRIBUTES[$blobName]["null"]){
-				throw new \UnexpectedValueException("El blob {$blobName} no es nullable.");	
+				throw new \UnexpectedValueException("Blob {$blobName} is not nullable.");
 			}
 			$blobResource = null;
 		}
-		// 4. Es un descriptor de fichero
+		// 4. File descriptor
 		elseif(get_resource_type($blob)==="stream"){
 			$blobResource = $blob;
 		}
-		// Si no es un tipo adecuado, da un error
+		// Otherwise, throw exception
 		else{
 			$blobType = gettype($blob);
-			throw new \UnexpectedValueException("El blob {$blobName} no es una cadena binaria, ni un array con 'path' como clave y la ruta del fichero, ni un descriptor de fichero. Es un {$blobType} que no sirve");
+			throw new \UnexpectedValueException("Blob {$blobName} is a {$blobType}, and that type is not allowed. Pass blob contents as a string, file path or stream");
 		}
 		
-		// Escritura del blob
+		// Writting the blob
 		$newValues = [$blobName => $blobResource];
 		$ok = $db::updateFields(static::TABLE_NAME, $newValues, $condition);
-		// Devolvemos si es correcto o no
 		return $ok;
 	}
 	
