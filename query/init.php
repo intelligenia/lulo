0655b412f5b04f54a66827fc81082147e53cc29d
Francisco Morales <francisco.morales@intelligenia.com>
2016-06-03 12:26:28 +0200

    remove twig requires in query init.php

--- query/init.php
+++ query/init.php
@@ -8,8 +8,5 @@ require_once LULO_DIR__QUERY."/proposition.class.php";
 require_once LULO_DIR__QUERY."/query.class.php";
 require_once LULO_DIR__QUERY."/transaction.class.php";
 require_once LULO_DIR__QUERY."/tuplevalue.class.php";
-require_once LULO_DIR__QUERY."/twigluloloader.class.php";
-require_once LULO_DIR__QUERY."/twigtemplate.class.php";
-
 
 ?>
